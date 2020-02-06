d2212784f321b557a02acb15808e4046f6824413
koiryu <53368783+koiryu@users.noreply.github.com>
2020-02-06 22:39:25 +0900

    for kaggle

--- decisionTree.py
+++ decisionTree.py
@@ -5,17 +5,28 @@ from IPython.display import Image
 from graphviz import Digraph
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('titanic.csv', sep='\t')
-print(df)
+df = pd.read_csv('train.csv')
+#print(df)
+df_test = pd.read_csv('test.csv')
+print(df_test)
+
+
 #df.fillna(df.mean())
-df2 = df.dropna(subset=['Age'])
+#df2 = df.dropna(subset=['Age'])
+df2 = df.fillna(df.median())
+#df_test2 = df_test.dropna(subset=['Age'])
+df_test2 = df_test.fillna(df_test.median())
 #print(df)
 df2['Sex'] = df2['Sex'].str.replace('female', '1')
 df2['Sex'] = df2['Sex'].str.replace('male', '0')
 df2['Sex'].astype(int)
 target = df2['Survived']
+df_test2['Sex'] = df_test2['Sex'].str.replace('female', '1')
+df_test2['Sex'] = df_test2['Sex'].str.replace('male', '0')
+df_test2['Sex'].astype(int)
 #print(target)
 data = df2[['Pclass', 'Sex', 'Age', 'SibSp']]
+test = df_test2[['Pclass', 'Sex', 'Age', 'SibSp']]
 #print(data)
 #print(df2.dtypes)
 
@@ -23,19 +34,14 @@ data = df2[['Pclass', 'Sex', 'Age', 'SibSp']]
 clf = tree.DecisionTreeClassifier(max_depth=3)  # limit depth of tree
 clf.fit(data, target)
 
-"""
-dot_data = tree.export_graphviz(
-    clf,
-    out_file=None,
-    feature_names=['Pclass', 'Sex', 'Age', 'SibSp'],
-    class_names=['Survived', 'not Survived'],
-    filled=True,
-    proportion=True)
-graph = pydotplus.graph_from_dot_data(dot_data)
-Image(graph.create_png())
-"""
 tree.plot_tree(clf,
               feature_names=['Pclass', 'Sex', 'Age', 'SibSp'],
               class_names=['Survived', 'not Survived'],
               filled=True, rounded=True, proportion=True)
 plt.savefig("tree1.4.png")
+
+#print(clf.predict(test))
+
+df_test2['Survived'] = clf.predict(test)
+
+df_test2.to_csv('predicted.csv', columns=['PassengerId', 'Survived'])
